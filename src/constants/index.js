841ccb9fe101ea34a52c29e01d39dca6551d2e57
radhakulkarni24 <radha.kulkarni024@gmail.com>
2024-09-07 17:04:24 +0530

    technology and experience section

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,40 +8,45 @@ delivering high-quality web content management solutions and user-
 friendly, responsive interfaces. My expertise includes HTML, CSS,
 JavaScript, and React, MERN stack. I am eager to contribute to innovative IT
 environments and collaborate with cross-functional teams to develop
-impactful web-applications.`
+impactful web-applications.`;
 
 export const ABOUT_TEXT = `I am Radha Kulkarni, a web developer with a passion for creating engaging and user-centric experiences. I completed Masters degree in Computer Applications from (MIT World Peace University) and completed my Bachlor's studies in Computer Applications.
 Throughout my career, I have good knowledge in cutting-edge technologies, specializing in frontend development. My toolkit includes the dynamic duo of (React.js), HTML, CSS, JavaScript, MongoDB allowing me to build robust and responsive web applications.
 Whether it's translating design concepts or create beautiful user interfaces, I take pride in my ability to turn ideas into reality. I am not just a developer; I am a problem solver, an innovator, and a dedicated professional ready to contribute 
-my skills to the ever-evolving landscape of web development.`
+my skills to the ever-evolving landscape of web development.`;
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "2024 - 2024",
+    role: "Application developer",
+    company: "Vendekin Technologies",
+    description: `Led the design and implementation of custom
+dashboards using React JS, improving performance and user experience.Developed modular, reusable
+components to enhance development efficiency and maintainability.`,
+    technologies: [
+      "React Js",
+      "TypeScript",
+      "HTML-CSS",
+      "Figma",
+      "Firebase",
+      "Google Cloud",
+    ],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "2024 - 2021",
+    role: "Web Developer",
+    company: "Celebal Technologies Private Limited",
+    description: `Web Application Development: Designed and maintained scalable web
+applications using React JS. Engineered responsive and visually
+appealing interfaces for web and mobile platforms, optimizing user
+experience.Utilized React to create dynamic,interactive interfaces, managing state and props effectively.Streamlined development processes, reducing
+average response time by 30% and increasing user engagement by 20%`,
+    technologies: [
+      "Bot Framework",
+      "Power platform",
+      "Node JS",
+      "React JS",
+      "MongoDB"
+    ],
   },
 ];
 
