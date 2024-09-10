fa5d619a398353fba363b269413736143352ad86
radhakulkarni24 <radha.kulkarni024@gmail.com>
2024-09-10 08:49:42 +0530

    navbar component name updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
-import NavBar from './components/NavBar';
+import NavBar from "./components/navBar";
 import Technology from "./components/Technology";
 import Experience from "./components/Experience";
 import Project from "./components/Projects";
